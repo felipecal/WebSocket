24801817bb7595b9cc2bedfc626e9da840b9dbef
Felipe <fcalldas@gmail.com>
2022-07-22 16:00:02 -0300

    ⚰️ removing dead code

--- src/socket/index.js
+++ src/socket/index.js
@@ -4,12 +4,8 @@ import { Server } from "socket.io";
 import path from 'path';
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
+const io = new Server(httpServer);
 const port = process.env.PORT || 3000;
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/'));
-// });
 
 
 app.use(express.static(path.join(__dirname, '..', "public")))
@@ -30,27 +26,28 @@ io.on('connection', (socket) => {
         if (index !== -1) {
             const user = { ...users[index], name };
             users[index] = user;
-            // io.emit(notifyMessage, `${user.name} with id: ${user.id}  is connected.`);
             io.emit(notifyMessage, `${user.name} is connected.`);
         }
     })
-    console.log(users);
+    socket.on('start-typing', function(name){
+        socket.broadcast.emit('start-typing', name)
+    })
+
+    socket.on('stop-typing', function(name){
+        socket.broadcast.emit('stop-typing', name)
+    })
 
     socket.on('disconnect', () => {
         const index = users.findIndex(element => element.id === socket.id);
         if (index !== -1) {
             const user = users[index];
-            // io.emit(notifyMessage, `${user.name} with id: ${user.id} is disconnected.`)
             io.emit(notifyMessage, `${user.name} is disconnected.`);
             users.splice(index, 1);
         }
 
     });
-    socket.on('typing', function (data) {
-        socket.broadcast.emit('typing', data)
-    })
+
     socket.on(chatMessage, (msg) => {
-        // io.emit(notifyMessage, msg);
         const index = users.findIndex(element => element.id === socket.id);
         const user = users[index];
         io.emit(notifyMessage, `${user.name}: ` + msg);
